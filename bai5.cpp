de881ee165a9468c510233cb5c1fde84634e8588
quyphandinh25-glitch <quyphandinh25@gmail.com>
2026-04-03 23:17:30 +0700

    Implement polynomial class with addition and subtraction
    Bài tập 5: Xét đa thức theo biến x (đa thức một biến) bậc n có dạng như sau: 
    P(X) = a1xn + a2xn-1 + a3xn-2 + … + aj 
    Trong đó: n là bậc của đa thức. a1, a2, a3,…, aj là các hệ số tương ứng với từng bậc của 
    đa thức. 
    Định nghĩa lớp cDaThuc biểu diễn khái niệm đa thức với các thao tác sau: - - - - - - 
    Khởi tạo một đa thức có bậc bằng 0 hoặc bậc. 
    Tính giá trị của đa thức khi biết giá trị của x 
    Nhập đa thức 
    Xuất đa thức 
    Cộng hai đa thức 
    Trừ hai đa thức

---
+++ bai5.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class cDaThuc {
+private:
+    int n;             
+    double a[100];     
+
+public:
+    cDaThuc() {
+        n = 0;
+        for (int i = 0; i < 100; i++) {
+            a[i] = 0;
+        }
+    }
+
+    void Nhap() {
+        cout << "Nhap bac cua da thuc n = ";
+        cin >> n;
+        
+        for (int i = n; i >= 0; i--) {
+            cout << "He so cua x^" << i << " = ";
+            cin >> a[i];
+        }
+    }
+
+    void Xuat() {
+        for (int i = n; i >= 0; i--) {
+            cout << a[i] << "x^" << i;
+            if (i > 0) {
+                cout << " + "; // In dấu cộng giữa các số
+            }
+        }
+        cout << "\n";
+    }
+
+    double TinhGiaTri(double x) {
+        double ketQua = 0;
+        for (int i = 0; i <= n; i++) {
+            ketQua += a[i] * pow(x, i); // pow(x, i) là x mũ i
+        }
+        return ketQua;
+    }
+
+    cDaThuc Cong(cDaThuc dt2) {
+        cDaThuc kq; // Tạo đa thức kết quả
+        kq.n = max(this->n, dt2.n); // Bậc kết quả là bậc lớn nhất của 2 đa thức
+
+        for (int i = 0; i <= kq.n; i++) {
+            kq.a[i] = this->a[i] + dt2.a[i];
+        }
+        return kq;
+    }
+
+    cDaThuc Tru(cDaThuc dt2) {
+        cDaThuc kq;
+        kq.n = max(this->n, dt2.n);
+
+        for (int i = 0; i <= kq.n; i++) {
+            kq.a[i] = this->a[i] - dt2.a[i];
+        }
+        return kq;
+    }
+};
+
+int main() {
+    cDaThuc P, Q, Tong, Hieu;
+
+    cout << "--- NHAP DA THUC P(x) ---\n";
+    P.Nhap();
+    
+    cout << "\n--- NHAP DA THUC Q(x) ---\n";
+    Q.Nhap();
+
+    cout << "\n--- KET QUA ---\n";
+    cout << "P(x) = "; P.Xuat();
+    cout << "Q(x) = "; Q.Xuat();
+
+    double x;
+    cout << "\nNhap x de tinh P(x): ";
+    cin >> x;
+    cout << "Gia tri P(" << x << ") = " << P.TinhGiaTri(x) << "\n";
+
+    Tong = P.Cong(Q);
+    cout << "\nP(x) + Q(x) = ";
+    Tong.Xuat();
+
+    Hieu = P.Tru(Q);
+    cout << "P(x) - Q(x) = ";
+    Hieu.Xuat();
+
+    return 0;
+}
