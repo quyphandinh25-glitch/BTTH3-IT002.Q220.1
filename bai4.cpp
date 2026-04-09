d2a3b8724cc965ef9165f5addb38b513df49c6bb
quyphandinh25-glitch <quyphandinh25@gmail.com>
2026-04-09 15:53:09 +0700

    Refactor cArray methods to use scope resolution

--- bai4.cpp
+++ bai4.cpp
@@ -7,145 +7,161 @@ private:
     int* arr;
     int n;
 
-    bool isPrime(int num) const {
-        if (num < 2) return false;
-        for (int i = 2; i <= sqrt(num); i++) {
-            if (num % i == 0) return false;
-        }
-        return true;
-    }
+    bool isPrime(int num) const;
+    void quickSort(int left, int right);
 
-    void quickSort(int left, int right) {
-        if (left >= right) return;
-        int pivot = arr[(left + right) / 2];
-        int i = left, j = right;
+public:  
+    cArray();
+    ~cArray();
 
-        while (i <= j) {
-            while (arr[i] < pivot) i++;
-            while (arr[j] > pivot) j--;
-            if (i <= j) {
-                swap(arr[i], arr[j]);
-                i++;
-                j--;
-            }
-        }
-        quickSort(left, j);
-        quickSort(i, right);
-    }
-
-public:
-    cArray() {
-        arr = nullptr;
-        n = 0;
-    }
-
-    ~cArray() {
-        if (arr != nullptr) {
-            delete[] arr;
-        }
-    }
-
-    void nhapMang(int size) {
-        if (arr != nullptr) {
-            delete[] arr;
-        }
-        n = size;
-        arr = new int[n];
-        cout << "Moi nhap " << n << " phan tu:\n";
-        for (int i = 0; i < n; i++) {
-            cout << "arr[" << i << "] = ";
-            cin >> arr[i];
-        }
-    }
-
-    void taoMangNgauNhien(int size) {
-        if (arr != nullptr) {
-            delete[] arr;
-        }
-        n = size;
-        arr = new int[n];
-        
-        srand(time(0)); 
-        for (int i = 0; i < n; i++) {
-            arr[i] = rand() % 100; 
-        }
-    }
-
-    void xuatMang() const {
-        if (n == 0) {
-            cout << "Mang rong!\n";
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            cout << arr[i] << " ";
-        }
-        cout << "\n";
-    }
-
-    int demSoLanXuatHien(int x) const {
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == x) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    bool kiemTraTangDan() const {
-        if (n <= 1) return true;
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] > arr[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    bool timPhanTuLeNhoNhat(int& res) const {
-        bool found = false;
-        int minOdd = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] % 2 != 0 && arr[i] < minOdd) {
-                minOdd = arr[i];
-                found = true;
-            }
-        }
-        if (found) res = minOdd;
-        return found;
-    }
-
-    bool timSoNguyenToLonNhat(int& res) const {
-        bool found = false;
-        int maxPrime = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            if (isPrime(arr[i]) && arr[i] > maxPrime) {
-                maxPrime = arr[i];
-                found = true;
-            }
-        }
-        if (found) res = maxPrime;
-        return found;
-    }
-
-    void sapXepTangDan() {
-        if (n > 1) {
-            quickSort(0, n - 1);
-        }
-    }
-
-    void sapXepGiamDan() {
-        for (int i = 0; i < n - 1; i++) {
-            int maxIdx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] > arr[maxIdx]) {
-                    maxIdx = j;
-                }
-            }
-            swap(arr[i], arr[maxIdx]);
-        }
-    }
+    void nhapMang(int size);
+    void taoMangNgauNhien(int size);
+    void xuatMang() const;
+    int demSoLanXuatHien(int x) const;
+    bool kiemTraTangDan() const;
+    bool timPhanTuLeNhoNhat(int& res) const;
+    bool timSoNguyenToLonNhat(int& res) const;
+    void sapXepTangDan();
+    void sapXepGiamDan();
 };
+
+bool cArray::isPrime(int num) const {
+    if (num < 2) return false;
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+void cArray::quickSort(int left, int right) {
+    if (left >= right) return;
+    int pivot = arr[(left + right) / 2];
+    int i = left, j = right;
+
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+    quickSort(left, j);
+    quickSort(i, right);
+}
+
+cArray::cArray() {
+    arr = nullptr;
+    n = 0;
+}
+
+cArray::~cArray() {
+    if (arr != nullptr) {
+        delete[] arr;
+    }
+}
+
+void cArray::nhapMang(int size) {
+    if (arr != nullptr) {
+        delete[] arr;
+    }
+    n = size;
+    arr = new int[n];
+    cout << "Moi nhap " << n << " phan tu:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+}
+
+void cArray::taoMangNgauNhien(int size) {
+    if (arr != nullptr) {
+        delete[] arr;
+    }
+    n = size;
+    arr = new int[n];
+    
+    srand(time(0)); 
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100; 
+    }
+}
+
+void cArray::xuatMang() const {
+    if (n == 0) {
+        cout << "Mang rong!\n";
+        return;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int cArray::demSoLanXuatHien(int x) const {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            count++;
+        }
+    }
+    return count;
+}
+
+bool cArray::kiemTraTangDan() const {
+    if (n <= 1) return true;
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool cArray::timPhanTuLeNhoNhat(int& res) const {
+    bool found = false;
+    int minOdd = INT_MAX;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] % 2 != 0 && arr[i] < minOdd) {
+            minOdd = arr[i];
+            found = true;
+        }
+    }
+    if (found) res = minOdd;
+    return found;
+}
+
+bool cArray::timSoNguyenToLonNhat(int& res) const {
+    bool found = false;
+    int maxPrime = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (isPrime(arr[i]) && arr[i] > maxPrime) {
+            maxPrime = arr[i];
+            found = true;
+        }
+    }
+    if (found) res = maxPrime;
+    return found;
+}
+
+void cArray::sapXepTangDan() {
+    if (n > 1) {
+        quickSort(0, n - 1);
+    }
+}
+
+void cArray::sapXepGiamDan() {
+    for (int i = 0; i < n - 1; i++) {
+        int maxIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[maxIdx]) {
+                maxIdx = j;
+            }
+        }
+        swap(arr[i], arr[maxIdx]);
+    }
+}
 
 void hienThiMenu() {
     cArray a;
@@ -224,7 +240,6 @@ void hienThiMenu() {
         }
     } while (luaChon != 0);
 }
-
 int main() {
     hienThiMenu();
     return 0;
