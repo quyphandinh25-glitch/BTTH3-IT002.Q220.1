e28f6b1889fc24a89dc0b85a14e75e0e181ea3ab
quyphandinh25-glitch <quyphandinh25@gmail.com>
2026-04-10 17:31:32 +0700

    Update bai5.cpp

--- bai5.cpp
+++ bai5.cpp
@@ -8,62 +8,70 @@ private:
     int n;             
     double a[100];     
 
-public:
-    cDaThuc() {
-        n = 0;
-        for (int i = 0; i < 100; i++) {
-            a[i] = 0;
-        }
-    }
-
-    void Nhap() {
-        cout << "Nhap bac cua da thuc n = ";
-        cin >> n;
-        
-        for (int i = n; i >= 0; i--) {
-            cout << "He so cua x^" << i << " = ";
-            cin >> a[i];
-        }
-    }
-
-    void Xuat() {
-        for (int i = n; i >= 0; i--) {
-            cout << a[i] << "x^" << i;
-            if (i > 0) {
-                cout << " + "; 
-            }
-        }
-        cout << "\n";
-    }
-
-    double TinhGiaTri(double x) {
-        double ketQua = 0;
-        for (int i = 0; i <= n; i++) {
-            ketQua += a[i] * pow(x, i); 
-        }
-        return ketQua;
-    }
-
-    cDaThuc Cong(cDaThuc dt2) {
-        cDaThuc kq; 
-        kq.n = max(this->n, dt2.n); 
-
-        for (int i = 0; i <= kq.n; i++) {
-            kq.a[i] = this->a[i] + dt2.a[i];
-        }
-        return kq;
-    }
-
-    cDaThuc Tru(cDaThuc dt2) {
-        cDaThuc kq;
-        kq.n = max(this->n, dt2.n);
-
-        for (int i = 0; i <= kq.n; i++) {
-            kq.a[i] = this->a[i] - dt2.a[i];
-        }
-        return kq;
-    }
+public: 
+    cDaThuc();
+    void Nhap();
+    void Xuat();
+    double TinhGiaTri(double x);
+    cDaThuc Cong(cDaThuc dt2);
+    cDaThuc Tru(cDaThuc dt2);
 };
+
+
+cDaThuc::cDaThuc() {
+    n = 0;
+    for (int i = 0; i < 100; i++) {
+        a[i] = 0;
+    }
+}
+
+void cDaThuc::Nhap() {
+    cout << "Nhap bac cua da thuc n = ";
+    cin >> n;
+    
+    for (int i = n; i >= 0; i--) {
+        cout << "He so cua x^" << i << " = ";
+        cin >> a[i];
+    }
+}
+
+void cDaThuc::Xuat() {
+    for (int i = n; i >= 0; i--) {
+        cout << a[i] << "x^" << i;
+        if (i > 0) {
+            cout << " + "; 
+        }
+    }
+    cout << "\n";
+}
+
+double cDaThuc::TinhGiaTri(double x) {
+    double ketQua = 0;
+    for (int i = 0; i <= n; i++) {
+        ketQua += a[i] * pow(x, i); 
+    }
+    return ketQua;
+}
+
+cDaThuc cDaThuc::Cong(cDaThuc dt2) {
+    cDaThuc kq;
+    kq.n = max(this->n, dt2.n); 
+
+    for (int i = 0; i <= kq.n; i++) {
+        kq.a[i] = this->a[i] + dt2.a[i];
+    }
+    return kq;
+}
+
+cDaThuc cDaThuc::Tru(cDaThuc dt2) {
+    cDaThuc kq;
+    kq.n = max(this->n, dt2.n);
+
+    for (int i = 0; i <= kq.n; i++) {
+        kq.a[i] = this->a[i] - dt2.a[i];
+    }
+    return kq;
+}
 
 int main() {
     cDaThuc P, Q, Tong, Hieu;
